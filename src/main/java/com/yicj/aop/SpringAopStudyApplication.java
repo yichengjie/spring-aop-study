819c6d338cee859cb6d463b4934a4b6c6981157c
易成杰 <626659321@qq.com>
2020-04-06 13:45:26 +0800

    保存

---
+++ src/main/java/com/yicj/aop/SpringAopStudyApplication.java
@@ -0,0 +1,13 @@
+package com.yicj.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yicj/aop/SpringAopStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yicj.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
