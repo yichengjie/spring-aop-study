19a58fb278b4b6465926383833a628101ae72f7c
易成杰 <626659321@qq.com>
2020-04-06 22:04:00 +0800

    保存

--- execution-study2/execution-stage1/src/main/java/com/yicj/service/ProductService.java
+++ execution-study2/execution-stage1/src/main/java/com/yicj/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.yicj.service;
 
 import com.yicj.domain.Product;
+import com.yicj.security.AdminOnly;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +11,15 @@ public class ProductService {
     @Autowired
     private AuthService authService ;
 
-
+    @AdminOnly
     public void insert(Product product){
-        authService.checkAccess();
+        //authService.checkAccess();
         System.out.println("insert product");
     }
 
+    @AdminOnly
     public void delete(Long id){
-        authService.checkAccess();
+        //authService.checkAccess();
         System.out.println("delete product");
     }
 }
