6d12d848dda3358aeda40a6ae22de94d22490259
易成杰 <626659321@qq.com>
2020-04-07 10:31:01 +0800

    保存

---
+++ execution-study2/execution-stage2/src/main/java/com/yicj/ExecutionApplication.java
@@ -0,0 +1,13 @@
+package com.yicj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutionApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ExecutionApplication.class,args) ;
+    }
+}
---
+++ execution-study2/execution-stage2/src/main/java/com/yicj/config/PkgTypeAspectConfig.java
@@ -0,0 +1,28 @@
+package com.yicj.config;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+/**
+ * //匹配ProductService类里头的所有方法
+ * @Poincut("within(com.yicj.service.ProductService)")
+ * //匹配com.yicj包及子包下所有类的方法
+ * @Pointcut("within(com.yicj..*)")
+ */
+@Aspect
+@Component
+public class PkgTypeAspectConfig {
+
+    @Pointcut("within(com.yicj.service.ProductService)")
+    public void matchType(){}
+
+    @Before("matchType()")
+    public void before(){
+
+        System.out.println();
+        System.out.println("### before");
+    }
+
+}
